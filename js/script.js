32892b36e4e415933e668a2197995aebbc6071d0
Fabio Claret <fabioclaret@gmail.com>
2024-10-24 10:36:51 -0300

    Coloquei comentarios no js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-//crio uma variavel que vai selecionar a imagem do mariogi
+//crio uma variavel que vai guardar a imagem do mario
 const mario = document.querySelector('.mario')
 
+//crio uma funcao seta (arrow function)
 const jump = () => {
+    //aqui ele vai adicionar a classe jump do css na imagem do mario fazendo ele pular
     mario.classList.add('jump');
     setTimeout(() => {
+        //removo a classe para poder colocar novamente ao clicar qualquer tecla
         mario.classList.remove('jump');
     }, 500);
 };
 
+//ao precionar a tecla, chamo a funcao seta jump
 document.addEventListener('keydown', jump)
