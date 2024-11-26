be8d3dfc7e3f3cbb6443e1b7f2479a8c2c499cfc
Fabio Claret <fabioclaret@gmail.com>
2024-11-26 17:59:46 -0300

    termino

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 //crio uma variavel que vai guardar a imagem do mario
 const mario = document.querySelector('.mario')
 
+//guardando a imagem em uma variavel pipe
+const pipe = document.querySelector('.pipe')
+
+//armazenando as nuvens em uma variavel 
+const cloud = document.querySelector('.clouds')
+
 //crio uma funcao seta (arrow function)
 const jump = () => {
     //aqui ele vai adicionar a classe jump do css na imagem do mario fazendo ele pular
@@ -13,3 +19,30 @@ const jump = () => {
 
 //ao precionar a tecla, chamo a funcao seta jump
 document.addEventListener('keydown', jump)
+
+const loop = setInterval(()=>{
+    console.log(loop)   
+    const pipePosition  = pipe.offsetLeft
+    const cloudPosition = cloud.offsetLeft
+    const marioPosition = +getComputedStyle(mario).bottom.replace('px',"")
+
+    console.log(marioPosition)
+  
+
+    if( pipePosition <= 120 && pipePosition > 0 && marioPosition < 110){
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        cloud.style.animation = 'none'
+        cloud.style.left = `${cloudPosition}px`
+
+        mario.src = './images/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+       
+        clearInterval(loop)
+    }     
+}, 10)
